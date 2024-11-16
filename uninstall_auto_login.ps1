8ed44a25172193130840731de164edf6db6583e8
LittlePeach996 <24120425@bjtu.edu.cn>
2024-11-16 14:48:17 +0800

    修改了弹出文本框的问题，将.ps1文件转移至user文件夹存储

--- uninstall_auto_login.ps1
+++ uninstall_auto_login.ps1
@@ -2,16 +2,17 @@
 $userName = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1]
 
 # 定义目标文件路径
+$userFolder = "C:\Users\$userName"
 $startupFolder = "C:\Users\$userName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-$targetFile = Join-Path -Path $startupFolder -ChildPath "LoginScript.ps1"
+$ps1File = Join-Path -Path $userFolder -ChildPath "LoginScript.ps1"
 $vbsFile = Join-Path -Path $startupFolder -ChildPath "RunLoginScript.vbs"
 
 # 检查并删除 PowerShell 脚本文件
-if (Test-Path -Path $targetFile) {
-    Remove-Item -Path $targetFile -Force
-    Write-Host "LoginScript.ps1 has been successfully removed from the startup folder." -ForegroundColor Green
+if (Test-Path -Path $ps1File) {
+    Remove-Item -Path $ps1File -Force
+    Write-Host "LoginScript.ps1 has been successfully removed from the user folder." -ForegroundColor Green
 } else {
-    Write-Host "LoginScript.ps1 not found in the startup folder." -ForegroundColor Yellow
+    Write-Host "LoginScript.ps1 not found in the user folder." -ForegroundColor Yellow
 }
 
 # 检查并删除 VBScript 文件
